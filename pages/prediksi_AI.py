36aec882430728860566271aa16b0a885262ff93
BasriNurSetyawan <setyawanwawan747@gmail.com>
2026-05-27 00:21:13 +0700

    Update prediksi_AI.py

--- pages/prediksi_AI.py
+++ pages/prediksi_AI.py
@@ -16,11 +16,11 @@ with c1:
     p_part_time = st.selectbox("Kerja Part-Time?", [0, 1], format_func=lambda x: "Ya" if x == 1 else "Tidak")
 with c2:
     p_screen = st.slider("Total Screen Time (Jam)", 0.0, 15.0, 4.0)
-    p_exercise = st.slider("Olahraga (Kali/Minggu)", 0, 6, 2)
+    p_exercise = st.slider("Olahraga (Kali/Minggu)", 0, 6, 3)
     p_eskul = st.selectbox("Ikut Ekstrakurikuler?", [0, 1], format_func=lambda x: "Ya" if x == 1 else "Tidak")
 with c3:
-    p_sleep = st.slider("Jam Tidur Harian", 3.0, 12.0, 7.0)
-    p_mental = st.slider("Kesehatan Mental (1-10)", 1, 10, 8)
+    p_sleep = st.slider("Jam Tidur Harian", 3.0, 12.0, 8.0)
+    p_mental = st.slider("Kesehatan Mental (1-10)", 1, 10, 7)
 
 input_df = pd.DataFrame([[p_study, p_screen, p_attend, p_sleep, p_mental, p_exercise, p_part_time, p_eskul]], columns=features)
 pred = model.predict(input_df)[0]
