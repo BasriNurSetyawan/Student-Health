04295c44c6d0dc21bbd540ed1d3e0b38fb1f4575
BasriNurSetyawan <setyawanwawan747@gmail.com>
2026-05-26 22:10:59 +0700

    Add data loading and model training functions
    Implement data loading and model training functions for burnout prediction.

---
+++ utils/helper.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import streamlit as st
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
+@st.cache_data
+def load_data():
+    try:
+        df = pd.read_csv('student_mental_health_burnout.csv')
+        # Buang data yang kosong dan dobel biar valid!
+        return df.dropna().drop_duplicates()
+    except FileNotFoundError:
+        return None
+
+@st.cache_resource
+def train_model(df):
+    # Fitur (kolom) yang dipakai buat nebak
+    features = ['age', 'daily_study_hours', 'daily_sleep_hours', 
+                'screen_time_hours', 'anxiety_score', 'depression_score', 
+                'academic_pressure_score', 'financial_stress_score']
+    
+    X = df[features].copy()
+    y = df['burnout_level'].copy() # Targetnya: Low, Medium, High
+    
+    le = LabelEncoder()
+    y_encoded = le.fit_transform(y)
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
+    
+    model = RandomForestClassifier(n_estimators=50, random_state=42, max_depth=5)
+    model.fit(X_train, y_train)
+    
+    akurasi = model.score(X_test, y_test)
+    
+    return model, le, features, akurasi
